ce712a1fbe14bd74977dc4589556ed58e2fc29b0
toprak4642 <sarikayatoprak418@gmail.com>
2024-05-19 22:32:57 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    clientSecret: "",
+    clientID: "BOT ID",
+    token: "",
+    mongo: "",
+    prefix: "*",
+    redirectURI: "http://ip:port/",
+    port: 40,
+    owners: ['ID'],
+    redirectionIP: ["http://ip:port"],
+    botlink: ["Davet Link"],
+    redirectionBot: ["https://www.discord.com/login"],
+    authlink: "",
+    webhookURL: "",
+   webhookURL2: ""
+}
